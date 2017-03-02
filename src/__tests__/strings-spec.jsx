68617c30989d5823c091fa99cc785d8a7bbe065f
Sepand Assadi <sepand.assadi@sony.com>
2017-03-02 12:20:38 -0800

    Adds tests for strings library

---
+++ src/__tests__/strings-spec.jsx
@@ -0,0 +1,163 @@
+/* eslint-disable */
+
+describe('Strings Library', () => {
+  describe('hash Method', () => {
+    const {hash} = require('../strings');
+
+    test('should return true if 32bit integer generated', () => {
+      let str = 'this is a sample string';
+      let result = hash(str);
+
+      expect(typeof str).toBe('string');
+      expect(typeof result).toBe('number');
+    });
+  });
+
+  describe('slugify Method', () => {
+    const {slugify} = require('../strings');
+
+    test('should return true when slugify method converts correctly', () => {
+      let str1 = 'THis Is A!   sample--string';
+      let str2 = 'this-is-a---sample--string';
+      let result = slugify(str1);
+
+      expect(result).not.toEqual(str1);
+      expect(result).toEqual(str2);
+    });
+  });
+
+  describe('String format Methods', () => {
+    const {capitalize, titleCase, camelCase, toUnderscoreCase} = require('../strings');
+
+    test('should return true when capitalize method converts correctly', () => {
+      let str1 = 'test test';
+      let str2 = 'Test Test';
+      let result = capitalize(str1);
+
+      expect(result).not.toEqual(str1);
+      expect(result).toEqual(str2);
+    });
+
+    test('should return true when titleCase method converts correctly', () => {
+      let str1 = 'this is a random string';
+      let str2 = 'This Is A Random String';
+      let result = titleCase(str1);
+
+      expect(result).not.toEqual(str1);
+      expect(result).toEqual(str2);
+    });
+
+    test('should return true when camelCase method converts correctly', () => {
+      let str1 = 'this is a random string';
+      let str2 = 'thisIsARandomString';
+      let result = camelCase(str1);
+
+      expect(result).not.toEqual(str1);
+      expect(result).toEqual(str2);
+    });
+
+    test('should return true when toUnderscoreCase method converts correctly', () => {
+      let str1 = 'this is a random string';
+      let str2 = 'THIS IS A RANDOM STRING';
+      let result = toUnderscoreCase(str1);
+
+      expect(result).not.toEqual(str1);
+      expect(result).toEqual(str2);
+    });
+  });
+
+  describe('generateRandomString Method', () => {
+    const {generateRandomString} = require('../strings');
+
+    test('should return true when generated the correct string length', () => {
+      let len1 = 5;
+      let len2 = 10;
+
+      let result1 = generateRandomString(len1);
+      let result2 = generateRandomString(len2);
+
+      expect(typeof result1).toEqual('string');
+      expect(result1.length).toEqual(len1);
+
+      expect(typeof result2).toEqual('string');
+      expect(result2.length).toEqual(len2);
+    });
+  });
+
+  describe('stringShortener Method', () => {
+    const {stringShortener} = require('../strings');
+
+    test('should return true when shorten the string correctly', () => {
+      let str = 'this is a sample text for testing';
+      let charCount = 10;
+
+      let result = stringShortener(str, charCount);
+      expect(typeof result).toEqual('string');
+      // Length would be charCount + ' ...' which is 4 chars
+      expect(result.length).toEqual(charCount + 4);
+      expect(result).not.toEqual(str);
+    });
+  });
+
+  describe('stringIsValid Method', () => {
+    const {stringIsValid} = require('../strings');
+
+    test('should return true when name format is valid', () => {
+      let name1 = 'Sam Assadi';
+      let name2 = 'Sam $Assadi@';
+
+      let result1 = stringIsValid('name', name1);
+      let result2 = stringIsValid('name', name2);
+
+      expect(result1).toEqual(true);
+      expect(result2).toEqual(false);
+    });
+    test('should return true when username format is valid', () => {
+      let username1 = 'sassadi9890';
+      let username2 = 'Sam Assadi';
+
+      let result1 = stringIsValid('username', username1);
+      let result2 = stringIsValid('username', username2);
+
+      expect(result1).toEqual(true);
+      expect(result2).toEqual(false);
+    });
+    test('should return true when email format is valid', () => {
+      let email1 = 'sepand.assadi@sony.com';
+      let email2 = 'sepandassadi.';
+      let email3 = 'sepandassadi.com';
+      let email4 = 'sepandassadi@';
+
+      let result1 = stringIsValid('email', email1);
+      let result2 = stringIsValid('email', email2);
+      let result3 = stringIsValid('email', email3);
+      let result4 = stringIsValid('email', email4);
+
+      expect(result1).toEqual(true);
+      expect(result2).toEqual(false);
+      expect(result3).toEqual(false);
+      expect(result4).toEqual(false);
+    });
+  });
+
+  describe('convertHTMLToString Method', () => {
+    const {convertHTMLToString} = require('../strings');
+
+    test('should return the value if not type of string', () => {
+      let num = 1;
+      let result = convertHTMLToString(num);
+
+      expect(typeof result).toEqual('number');
+      expect(result).toEqual(num);
+    });
+
+    test('should return true when HTML tags are removed from string', () => {
+      let str = '<p>this is a text for<span>testing</span></p>';
+      let result = convertHTMLToString(str);
+
+      expect(result).not.toEqual(str);
+      expect(result).toEqual('this is a text fortesting');
+    });
+  });
+
+});
