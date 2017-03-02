7e92253ea76ca4c18a950c4bb6ad34bc62a85572
Sepand Assadi <sepand.assadi@sony.com>
2017-03-02 13:03:40 -0800

    Updates the sort tests

--- src/__tests__/sort-spec.jsx
+++ src/__tests__/sort-spec.jsx
@@ -15,4 +15,55 @@ describe('Sort Library', () => {
       expect(result).toEqual(diff);
     });
   });
+  describe('Sort Methods', () => {
+    const {sortBy, sortByDates} = require('../sort');
+
+    test('should return true if sorted by dates correctly', () => {
+      let arr1 = [{
+        name: 'sample1',
+        desc: 'disc1',
+        updated_at: Date.now()
+      }, {
+        name: 'sample2',
+        desc: 'disc2',
+        updated_at: Date.now() - 100
+      }, {
+        name: 'sample3',
+        desc: 'disc3',
+        updated_at: Date.now() - 50
+      }, {
+        name: 'sample4',
+        desc: 'disc4',
+        updated_at: Date.now() + 20
+      }];
+
+      let arr2 = Object.assign({}, arr1);
+      arr1.sort(sortByDates('-updated_at'));
+
+      expect(arr2).not.toEqual(arr1);
+      expect(arr1[0].name).toEqual('sample4');
+    });
+
+    test('should return true if sorted by correctly', () => {
+      let arr1 = [{
+        name: 'sample1',
+        age: 40
+      }, {
+        name: 'sample2',
+        age: 30
+      }, {
+        name: 'sample3',
+        age: 80
+      }, {
+        name: 'sample4',
+        age: 20
+      }];
+
+      let arr2 = Object.assign({}, arr1);
+      arr1.sort(sortBy('-age')).reverse();
+
+      expect(arr2).not.toEqual(arr1);
+      expect(arr1[0].name).toEqual('sample4');
+    });
+  });
 });
